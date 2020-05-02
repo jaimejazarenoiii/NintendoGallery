b2e1dc19175754d2d5b9befae807571d64dd7d0b
Jaime Jazareno III <jaimejazarenoiii@gmail.com>
2020-05-03 06:12:37 +0800

    Created enums

---
+++ NintendoGallery/Enums/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  NintendoGallery
+//
+//  Created by Jaime Jazareno III on 5/3/20.
+//  Copyright © 2020 Jaime Jazareno III. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum Constants {
+    static var screenWidth = UIScreen.main.bounds.width
+    static var screenHeight = UIScreen.main.bounds.height
+    static var baseUrl = "https://www.amiiboapi.com/"
+}
