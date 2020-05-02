cb723696a89384a80837dbee3698b1b5a480cd39
Jaime Jazareno III <jaimejazarenoiii@gmail.com>
2020-05-03 02:30:05 +0800

    Initial Commit

---
+++ NintendoGallery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NintendoGallery
+//
+//  Created by Jaime Jazareno III on 5/3/20.
+//  Copyright © 2020 Jaime Jazareno III. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
